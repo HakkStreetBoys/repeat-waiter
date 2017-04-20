89f3273bbf720a3dbc0b50da57b61c5a00269001
palmimar <palmi@Palmis-MacBook-Pro.local>
2017-04-20 13:41:18 +0000

    Firebase test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,41 @@
+// import Firebase from './firebase';
+// import _ from 'lodash';
+// import {
+//   FETCH_POSTS,
+//   DELETE_POST,
+//   CREATE_POST
+// } from './types';
+//
+// // Initialize Firebase
+// // export const config = firebase.initializeApp({
+// //   apiKey: "AIzaSyCe4xhBRH01-H-jJkzBxRd9Mcxm8Et9D6g",
+// //   authDomain: "repeat-bc71f.firebaseapp.com",
+// //   databaseURL: "https://repeat-bc71f.firebaseio.com",
+// //   projectId: "repeat-bc71f",
+// //   storageBucket: "repeat-bc71f.appspot.com",
+// //   messagingSenderId: "945144509256"
+// // });
+// var ref = config.database('https://one-time-password-c0c13.firebaseio.com').ref();
+//
+// export function fetchPosts() {
+//   return dispatch => {
+//     ref.on('value', snapshot => {
+//       dispatch({
+//         type: FETCH_POSTS,
+//         payload: snapshot.val()
+//       });
+//     });
+//   };
+// }
+//
+// export function createPost(post) {
+//   return dispatch => Posts.push(post);
+// }
+//
+// export function deletePost(key) {
+//   return dispatch => Posts.child(key).remove();
+// }
+
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
---
+++ src/components/Login.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { Link } from "react-router";
+import {
+  Button,
+  ButtonToolbar,
+  FormControl,
+  Form,
+  FormGroup,
+  Alert
+} from "react-bootstrap";
+
+import firebase from "./firebase";
+
+const Presentation = ({ errorMessage, isLoggingIn, ...props }) => (
+  <Form onSubmit={props.handleSubmit}>
+    {errorMessage && <Alert bsStyle="danger">{errorMessage}</Alert>}
+    <FormGroup>
+      <FormControl
+        type="text"
+        placeholder="Email"
+        value={props.email}
+        onChange={props.handleEmailChange}
+      />
+    </FormGroup>
+    <FormGroup>
+      <FormControl
+        type="password"
+        placeholder="Password"
+        value={props.password}
+        onChange={props.handlePasswordChange}
+      />
+    </FormGroup>
+    <ButtonToolbar>
+      <Button type="submit" bsStyle="primary" disabled={isLoggingIn}>
+        {isLoggingIn ? "Logging in..." : "Login"}
+      </Button>
+      <Link className="btn btn-link" to="/register">Register</Link>
+    </ButtonToolbar>
+  </Form>
+);
+
+export default React.createClass({
+  getInitialState() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  render() {
+    const { state } = this;
+
+    return (
+      <Presentation
+        errorMessage={state.errorMessage}
+        isLoggingIn={state.isLoggingIn}
+        handleSubmit={this.handleSubmit}
+        email={state.email}
+        handleEmailChange={this.handleEmailChange}
+        password={state.password}
+        handlePasswordChange={this.handlePasswordChange}
+      />
+    );
+  },
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value });
+  },
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value });
+  },
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({ isLoggingIn: true });
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .catch(error => {
+        this.setState({ errorMessage: error.message, isLoggingIn: false });
+      });
+  }
+});
