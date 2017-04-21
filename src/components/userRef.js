74b8388228409d35bc4e39144fcdf73a2892ff3c
palmimar <palmi@Palmis-MacBook-Pro.local>
2017-04-21 14:11:36 +0000

    Firebase tenging náð

--- src/components/userRef.js
+++
@@ -1,8 +0,0 @@
-import firebase from "./firebase";
-
-/**
- * Gets the Firebase database ref for a particular user
- */
-export default function(user) {
-  return firebase.database().ref("users").child(user+'/');
-}
