5e148435a3029b0796bc32ee9788486d506ab8f7
ekonrads <kh.krimper@gmail.com>
2017-05-10 14:17:00 +0000

    fixes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,7 @@
 import React, { Component } from 'react'
 import firebase from './firebase'
-class Button extends Component {
-	render() {
-		var clicked = this.state && this.state.clicked
-		const status = ['new-order', 'in-prosess', 'complete']
-		const text = ['Ný Pöntun', 'Í Vinnslu', 'Afgreitt']
-		const clickedInfo = clicked ? 'clicked' : undefined
-		const cName = clickedInfo + ' ' + status[this.props.status_item]
-		return (
-			<div className={'button ' + cName} onClick={this.onClick.bind(this)}>
-				{text[this.props.status_item]}
-			</div>
-		)
-	}
 
+class Button extends Component {
 	onClick() {
 		let temp = this.props.status_item
 		if (temp < 2) {
@@ -21,19 +9,38 @@ class Button extends Component {
 		} else {
 			temp = 0
 		}
-		if(this.props.button_type) {
-		for (let keyStatus in this.props.key3) {
-			firebase
-			.database()
-			.ref(
-				'users/' +
-					this.props.key2 +
-					'/confirmed_order/' +
-					this.props.key1 +
-					'/' +
-					this.props.key3[keyStatus]
-			).once("value", snapshot => {
-				if(this.props.button_type === snapshot.val().category) {
+		if (this.props.button_type) {
+			for (let keyStatus in this.props.key3) {
+				firebase
+					.database()
+					.ref(
+						'users/' +
+							this.props.key2 +
+							'/confirmed_order/' +
+							this.props.key1 +
+							'/' +
+							this.props.key3[keyStatus]
+					)
+					.once('value', snapshot => {
+						if (this.props.button_type === snapshot.val().category) {
+							firebase
+								.database()
+								.ref(
+									'users/' +
+										this.props.key2 +
+										'/confirmed_order/' +
+										this.props.key1 +
+										'/' +
+										this.props.key3[keyStatus]
+								)
+								.update({
+									status_item: temp,
+								})
+						}
+					})
+			}
+		} else {
+			for (let keyStatus in this.props.key3) {
 				firebase
 					.database()
 					.ref(
@@ -48,28 +55,21 @@ class Button extends Component {
 						status_item: temp,
 					})
 			}
-			})
-	}
-} else {
-	for (let keyStatus in this.props.key3) {
-		firebase
-			.database()
-			.ref(
-				'users/' +
-					this.props.key2 +
-					'/confirmed_order/' +
-					this.props.key1 +
-					'/' +
-					this.props.key3[keyStatus]
-			)
-			.update({
-						status_item: temp,
-					})
+		}
 	}
 
-}
-
-}
+	render() {
+		var clicked = this.state && this.state.clicked
+		const status = ['new-order', 'in-prosess', 'complete']
+		const text = ['Ný Pöntun', 'Í Vinnslu', 'Afgreitt']
+		const clickedInfo = clicked ? 'clicked' : undefined
+		const cName = clickedInfo + ' ' + status[this.props.status_item]
+		return (
+			<div className={'button ' + cName} onClick={this.onClick.bind(this)}>
+				{text[this.props.status_item]}
+			</div>
+		)
+	}
 }
 
 export default Button
